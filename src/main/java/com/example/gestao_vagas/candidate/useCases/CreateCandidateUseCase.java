57e52decbe0a95fa52a553ddbf7764b2dcd0de11
Arthur <arthur.fabricyo@gmail.com>
2026-06-19 14:04:04 -0200

    first commit

---
+++ src/main/java/com/example/gestao_vagas/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,23 @@
+package com.example.gestao_vagas.candidate.useCases;
+
+import com.example.gestao_vagas.candidate.CandidateEntity;
+import com.example.gestao_vagas.candidate.CandidateRepository;
+import com.example.gestao_vagas.candidate.exceptions.UserFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public CandidateEntity execute(CandidateEntity candidateEntity){
+        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user)->{
+                    throw new UserFoundException();
+                });
+        return this.candidateRepository.save(candidateEntity);
+    }
+}
+
