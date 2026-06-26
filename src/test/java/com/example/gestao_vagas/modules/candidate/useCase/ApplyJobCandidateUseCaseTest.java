6b670519bb2e921e239b3967c0917d6908f541a3
Arthur <arthur.fabricyo@gmail.com>
2026-06-26 10:10:45 -0200

    adding tests in good case to aplly job candidate use case

---
+++ src/test/java/com/example/gestao_vagas/modules/candidate/useCase/ApplyJobCandidateUseCaseTest.java
@@ -0,0 +1,61 @@
+package com.example.gestao_vagas.modules.candidate.useCase;
+
+import com.example.gestao_vagas.modules.candidate.CandidateEntity;
+import com.example.gestao_vagas.modules.candidate.CandidateRepository;
+import com.example.gestao_vagas.modules.candidate.exceptions.JobNotFoundException;
+import com.example.gestao_vagas.modules.candidate.exceptions.UserNotFoundException;
+import com.example.gestao_vagas.modules.candidate.useCases.ApplyJobCandidateUseCase;
+import com.example.gestao_vagas.modules.company.entities.repositories.JobRepository;
+import org.hibernate.mapping.Any;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplyJobCandidateUseCaseTest {
+
+    @InjectMocks
+    private ApplyJobCandidateUseCase applyJobCandidateUseCase;
+
+    @Mock
+    private CandidateRepository candidateRepository;
+
+    @Mock
+    private JobRepository jobRepository;
+
+    @Test
+    @DisplayName("Should not be able to apply job with candidate not found")
+    public void shouldNotBeAbleToApplyJobWithCandidateNotFound(){
+        try {
+            applyJobCandidateUseCase.execute(null, null);
+        } catch (Exception e){
+            assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+    }
+
+    @Test
+    public void shouldNotBeAbleToApplyJobWithJobNotFound(){
+        var idCandidate = UUID.randomUUID();
+        var candidate = new CandidateEntity();
+        candidate.setId(idCandidate);
+        when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidate));
+
+        try{
+            applyJobCandidateUseCase.execute(idCandidate, null);
+
+        }catch (Exception e){
+            assertThat(e).isInstanceOf(JobNotFoundException.class);
+        }
+    }
+
+
+}
